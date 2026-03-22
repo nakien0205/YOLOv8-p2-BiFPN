2000c1be74998fa5e3bb671439008471ec969054
nakien0205 <phongkhiem.waseco@gmail.com>
2026-03-22 10:46:03 +0700

    modified:   tests/test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
-model = YOLO("ultralytics/cfg/models/v8/yolov8-p2-ghost.yaml")
+model = YOLO("ultralytics/cfg/models/v8/yolov8-p2-BiFPN.yaml")
 # model.info()
 model.info()
